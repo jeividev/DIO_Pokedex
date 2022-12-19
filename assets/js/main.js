e08b828ee3372e65e9ffa71ad9cf219537f2f3c6
José Vitor <josevytorsilva@hotmail.com>
2022-12-19 06:22:18 -0300

    CSS tweaks and pagination engine enhancement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,45 @@
-function pokemonToHtml(pokemon, index, array) {
-    function convertPokemonToNumber(id) {
-        const padraoId = "000"
-        return "#" + padraoId.substring(0, padraoId.length - id.toString().length) + id.toString()
+const pokemonMenuHtmlOl = document.getElementById("pokemonsList")
+const loadMoreButton = document.getElementById("loadMoreButton")
+
+const limit = 5
+let offset = 0
+
+loadMorePokemos = (offset, limit) => {
+    function pokemonToHtml(pokemon) {
+        function convertPokemonToNumber(id) {
+            const padraoId = "000"
+            return "#" + padraoId.substring(0, padraoId.length - id.toString().length) + id.toString()
+        }
+
+        return `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">${convertPokemonToNumber(pokemon.number)}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}"> ${type}</li>`).join(" ")}
+                    </ol>
+    
+                    <img
+                        src=${pokemon.photo}
+                        alt="${pokemon.name}"
+                    />
+                </div>
+            </li>
+        `
     }
 
-    return `
-        <li class="pokemon">
-            <span class="number">${convertPokemonToNumber(pokemon.number)}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type"> ${type}</li>`).join(" ")}
-                </ol>
-
-                <img
-                    src=${pokemon.photo}
-                    alt="${pokemon.name}"
-                />
-            </div>
-        </li>
-    `
+    pokeApi.getPokemons(offset, limit).then((dataJson = []) => {
+        console.log(dataJson)
+        pokemonMenuHtmlOl.innerHTML += dataJson.map(pokemonToHtml).join("")
+    })
 }
 
-const pokemonMenuHtmlOl = document.getElementById("pokemonsList")
+loadMorePokemos()
 
-pokeApi.getPokemons().then((dataJson = []) => {
-    console.log(dataJson)
-    pokemonMenuHtmlOl.innerHTML += dataJson.map(pokemonToHtml).join("")
+loadMoreButton.addEventListener('click', () =>{
+    offset += limit
+    loadMorePokemos(offset, limit)
+
+
 })
